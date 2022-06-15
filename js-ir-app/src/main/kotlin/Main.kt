2ce847e923ebcc06d186a27a8358f1fbba19d2a9
IKupriyanov-HORIS <ikupriyanov@horis.ru>
2022-06-15 16:48:06 +0300

    Cleanup

--- js-ir-app/src/main/kotlin/Main.kt
+++ js-ir-app/src/main/kotlin/Main.kt
@@ -1,16 +1,5 @@
-/*
- * Copyright (c) 2021. JetBrains s.r.o.
- * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
- */
-
 import kotlinx.browser.document
 import kotlinx.browser.window
-import org.w3c.dom.HTMLButtonElement
-import kotlin.math.PI
-import kotlin.math.cos
-import kotlin.math.ln
-import kotlin.math.sqrt
-import kotlin.random.Random
 
 fun main() {
     window.onload = { createContent() }
